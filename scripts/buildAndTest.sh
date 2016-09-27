3da6dbdccab4ff343271e8d89ddd89a37d532dc1
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2016-09-27 17:01:00 +0300

    Fixing Travis

--- scripts/buildAndTest.sh
+++ scripts/buildAndTest.sh
@@ -2,5 +2,23 @@
 
 mkdir -p  build
 cd build
-#cmake  -DCMAKE_CXX_FLAGS=-std=c++14 -DADD_G3LOG_UNIT_TEST=ON ..
-#make -j
+
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gmock/src/gmock.o ../contrib/gmock/src/gmock.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest-test-part.o ../contrib/gtest/src/gtest-test-part.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gmock/src/gmock-internal-utils.o ../contrib/gmock/src/gmock-internal-utils.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest-port.o ../contrib/gtest/src/gtest-port.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest-printers.o ../contrib/gtest/src/gtest-printers.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gmock/src/gmock-spec-builders.o ../contrib/gmock/src/gmock-spec-builders.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest.o ../contrib/gtest/src/gtest.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o test/conwrap/ProcessorAsio.o ../test/conwrap/ProcessorAsio.cpp 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest-typed-test.o ../contrib/gtest/src/gtest-typed-test.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o test/conwrap/ProcessorQueue.o ../test/conwrap/ProcessorQueue.cpp 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gmock/src/gmock-cardinalities.o ../contrib/gmock/src/gmock-cardinalities.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest-filepath.o ../contrib/gtest/src/gtest-filepath.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o test/main.o ../test/main.cpp 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gmock/src/gmock-matchers.o ../contrib/gmock/src/gmock-matchers.cc 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o test/conwrap/Processor.o ../test/conwrap/Processor.cpp 
+g++ -std=c++1y -I../src/include -I../contrib/gmock/include -I../contrib/gmock -I../contrib/gtest/include -I../contrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o contrib/gtest/src/gtest-death-test.o ../contrib/gtest/src/gtest-death-test.cc 
+
+g++ -o conwrap contrib/gmock/src/gmock-cardinalities.o contrib/gmock/src/gmock-internal-utils.o contrib/gmock/src/gmock-matchers.o contrib/gmock/src/gmock-spec-builders.o contrib/gmock/src/gmock.o contrib/gtest/src/gtest-death-test.o contrib/gtest/src/gtest-filepath.o contrib/gtest/src/gtest-port.o contrib/gtest/src/gtest-printers.o contrib/gtest/src/gtest-test-part.o contrib/gtest/src/gtest-typed-test.o contrib/gtest/src/gtest.o test/conwrap/Processor.o test/conwrap/ProcessorAsio.o test/conwrap/ProcessorQueue.o test/main.o -lboost_system -lpthread 
+
