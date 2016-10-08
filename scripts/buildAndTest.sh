7276471ed32dd25ddf45eefdafa58d4652970861
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2016-10-08 23:45:59 +0300

    Travis script fix

--- scripts/buildAndTest.sh
+++ scripts/buildAndTest.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
 
 mkdir -p  build
-
-gcc -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gmock.o contrib/gmock/src/gmock.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest-test-part.o contrib/gtest/src/gtest-test-part.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gmock-internal-utils.o contrib/gmock/src/gmock-internal-utils.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest-port.o contrib/gtest/src/gtest-port.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest-printers.o contrib/gtest/src/gtest-printers.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gmock-spec-builders.o contrib/gmock/src/gmock-spec-builders.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest.o contrib/gtest/src/gtest.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/ProcessorAsio.o test/conwrap/ProcessorAsio.cpp 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest-typed-test.o contrib/gtest/src/gtest-typed-test.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/ProcessorQueue.o test/conwrap/ProcessorQueue.cpp 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gmock-cardinalities.o contrib/gmock/src/gmock-cardinalities.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest-filepath.o contrib/gtest/src/gtest-filepath.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/main.o test/main.cpp 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gmock-matchers.o contrib/gmock/src/gmock-matchers.cc 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/Processor.o test/conwrap/Processor.cpp 
-g++ -std=c++1y -Isrc/include -Icontrib/gmock/include -Icontrib/gmock -Icontrib/gtest/include -Icontrib/gtest -O0 -g3 -Wall -c -fmessage-length=0 -o build/gtest-death-test.o contrib/gtest/src/gtest-death-test.cc 
-
-g++ -o build/conwrap build/gmock-cardinalities.o build/gmock-internal-utils.o build/gmock-matchers.o build/gmock-spec-builders.o build/gmock.o build/gtest-death-test.o build/gtest-filepath.o build/gtest-port.o build/gtest-printers.o build/gtest-test-part.o build/gtest-typed-test.o build/gtest.o build/Processor.o build/ProcessorAsio.o build/ProcessorQueue.o build/main.o -lboost_system -lpthread 
-
 cd  build
+
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o main.o test/main.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o ProcessorQueue.o test/conwrap/ProcessorQueue.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o ProcessorAsio.o test/conwrap/ProcessorAsio.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o Processor.o test/conwrap/Processor.cpp 
+
+g++ -Lcontrib/googletest/build/googlemock -Lcontrib/googletest/build/googletest -o conwrap Processor.o ProcessorAsio.o ProcessorQueue.o main.o -lpthread -lgmock -lgtest 
+
 ./conwrap
