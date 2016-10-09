b4d6b04ab41e0e20dad36741a2c15edc21f6e2f9
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2016-10-09 12:57:48 +0300

    Travis script fix

--- scripts/buildAndTest.sh
+++ scripts/buildAndTest.sh
@@ -14,13 +14,14 @@ cd  ../googletest
 cmake ../../googletest
 make
 
+cd  ../../../..
 mkdir -p  build
 cd  build
 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o main.o test/main.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o ProcessorQueue.o test/conwrap/ProcessorQueue.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o ProcessorAsio.o test/conwrap/ProcessorAsio.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o Processor.o test/conwrap/Processor.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o main.o ../test/main.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o ProcessorQueue.o ../test/conwrap/ProcessorQueue.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o ProcessorAsio.o ../test/conwrap/ProcessorAsio.cpp 
+g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o Processor.o ../test/conwrap/Processor.cpp 
 
 g++ -Lcontrib/googletest/build/googlemock -Lcontrib/googletest/build/googletest -o conwrap Processor.o ProcessorAsio.o ProcessorQueue.o main.o -lpthread -lgmock -lgtest 
 
