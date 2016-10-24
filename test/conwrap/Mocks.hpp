a7ac73ca97d7b71a8088b7c3900d7ef2dd2d2001
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2016-10-24 22:49:45 +0300

    Refactoring

--- test/conwrap/Mocks.hpp
+++ test/conwrap/Mocks.hpp
@@ -162,7 +162,7 @@ namespace conwrap
 		protected:
 			virtual HandlerContext<Dummy> createContext() override
 			{
-				return processorBasePtr->createHandlerContext();
+				return processorBasePtr->createContext();
 			}
 
 			virtual HandlerWrapper wrapHandler(std::function<void()> handler, bool proxy) override
