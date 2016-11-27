94b353704fc58d2083d43cb2c593af8a903c028d
Andrej Kislovskij <gimesketvirtadieni@gmail.com>
2016-11-27 13:24:57 +0200

    Switching to make file for Travis build

--- scripts/buildAndTest.sh
+++ scripts/buildAndTest.sh
@@ -14,14 +14,7 @@ cd  ../googletest
 cmake ../../googletest
 make
 
-cd  ../../../..
-mkdir -p  build
+cd  ../../../../make
+make tests
 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o build/ProcessorQueue.o test/conwrap/ProcessorQueue.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o build/main.o test/main.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o build/Processor.o test/conwrap/Processor.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o build/ProcessorAsio.o test/conwrap/ProcessorAsio.cpp 
-g++ -std=c++1y -DASIO_STANDALONE -Isrc/include -Icontrib/asio/asio/include -Icontrib/googletest/googlemock/include -Icontrib/googletest/googletest/include -O0 -g3 -Wall -c -fmessage-length=0 -o build/ProcessorCommon.o test/conwrap/ProcessorCommon.cpp 
-g++ -Lcontrib/googletest/build/googlemock -Lcontrib/googletest/build/googletest -o build/conwrap build/Processor.o build/ProcessorAsio.o build/ProcessorCommon.o build/ProcessorQueue.o build/main.o -lpthread -lgmock -lgtest 
-
-./build/conwrap
+./conwrap
